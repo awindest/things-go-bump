80681678eb2c6d895f75d4ba20ba92d4cabbb696
awindest <william.indest@gmail.com>
2025-09-13 16:25:53 -0400

    Initial version - WEBGPU

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
+
+
+<style>
+  :global(body, html) {
+    margin: 0;
+  }
+
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Canvas, extend } from '@threlte/core'
+  import Scene from './Scene.svelte'
+  import * as THREE from 'three/webgpu'
+
+  extend(THREE)
+</script>
+
+<div>
+  <Canvas
+    createRenderer={(canvas) => {
+      return new THREE.WebGPURenderer({
+        canvas,
+        antialias: true,
+        forceWebGL: false
+      })
+    }}
+  >
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  div {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+</style>
